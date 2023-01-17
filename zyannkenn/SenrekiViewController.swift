a39867d6a5a0b92f2e5dc71ca3cf639ae0b9bd85
sakihosonuma <chipdale411@gmail.com>
2023-01-17 20:18:24 +0900

    a

--- zyannkenn/SenrekiViewController.swift
+++ zyannkenn/SenrekiViewController.swift
@@ -14,18 +14,22 @@ class SenrekiViewController: UIViewController, UITableViewDelegate, UITableViewD
     var syouhai : Int = 0
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        // print(syouhai)
-        //label.text = String(syouhai)
+       label.text = String(syouhai)
         
-        TableView.reloadData()
+      
 
         // Do any additional setup after loading the view.
     }
-   
+    override func viewWillAppear(_ animated: Bool) {
+          super.viewWillAppear(animated)
+        TableView.reloadData()
+        }
     
-
+    
     /*
     // MARK: - Navigation
 
@@ -39,7 +43,7 @@ class SenrekiViewController: UIViewController, UITableViewDelegate, UITableViewD
     
    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+       return 1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -49,7 +53,7 @@ class SenrekiViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
 
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    /*func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "katicell", for: indexPath)
 
         // セルを取得する
@@ -63,8 +67,25 @@ class SenrekiViewController: UIViewController, UITableViewDelegate, UITableViewD
             cell.textLabel!.text = "勝ち"
                         }
         // セルに表示する値を設定する
-   
+        print(syouhai)
         return cell
     }
-
+     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "katicell", for: indexPath)
+        
+        if syouhai == 1{
+            cell.textLabel!.text = "あいこ"
+                        }
+        if syouhai == 2{
+            cell.textLabel!.text = "負け"
+                        }
+        if syouhai == 3{
+            cell.textLabel!.text = "勝ち"
+        }
+        // セルに表示する値を設定する
+        print(syouhai)
+        return cell
+    }
 }
